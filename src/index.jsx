8d06383bf27384a6dd6fa4eba83fac53e608f1b4
Morgan Greff <morgan@greffs.com>
2017-10-20 11:24:23 -0600

    Added MDL imports

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,5 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-
+import "../node_modules/material-design-lite/material.min.js"
 ReactDOM.render(<App />, document.getElementById('react-root'));
