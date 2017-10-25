1bd63d6317f03acfcea0e36d4445781c34bace86
Arie Gerritse <gerritse.arie@gmail.com>
2017-10-25 17:04:32 -0600

    Added Dots

--- src/presentational/stepOne.jsx
+++ src/presentational/stepOne.jsx
@@ -21,28 +21,30 @@ change(event) {
 
     render() {
       return (
+        <section id="StepOne">
         <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="box text-center purple vert-center">
-                <div className="row">
-                  <div className="col-md-12">
-                <h1>Learn The block chain!</h1>
-                <h2>Enter You Personal Secret Key</h2>
-                </div>
-                <div className="col-md-12">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <label className="mdl-textfield__label" htmlFor="sample1">
-                    privateKey
-                  </label>
-                  <input className="mdl-textfield__input" type="text" onChange={ this.change } value={ this.props.privKey }/>
-                </div>
-              </div>
-              </div>
-              </div>
-            </div>
-          </div>
+        <div className="row">
+        <div className="col-md-12">
+        <div className="box text-center purple vert-center">
+        <div className="row">
+        <div className="col-md-12">
+        <h1>Learn The block chain!</h1>
+        <h2>Enter You Personal Secret Key</h2>
         </div>
+        <div className="col-md-12">
+        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <label className="mdl-textfield__label" htmlFor="sample1">
+        privateKey
+        </label>
+        <input className="mdl-textfield__input" type="text" onChange={ this.change } value={ this.props.privKey }/>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </section>
       )
     }
   }
--- src/presentational/stepSix.jsx
+++ src/presentational/stepSix.jsx
@@ -25,109 +25,103 @@ class StepSix extends Component {
 
   render() {
     return (
-      <div
-        data-aos="fade-up"
-        data-aos-delay="1000"
-        className="container"
-        id="server-animation">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <div className="line"/>
-            <h1>SEND BLOCK</h1>
-            <div className="line"/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-8">
-            <div className="box stepSix">
-              <div className="row">
-                <div className="vert-center">
-                  <div className="col-md-8 text-center secondaryFont">
-                    <button
-                      href="#"
-                      className="play"
-                      onClick={this
-                      .playAnimation
-                      .bind(this)}>
-                      Play Animation
-                    </button>
-                    <button
-                      href="#"
-                      className="restart hidden"
-                      onClick={this
-                      .restartAnimation
-                      .bind(this)}>
-                      Restart Animation
-                    </button>
-                    <i className="material-icons server1 pause-animation">
-                      storage
-                    </i>
-                    <div className="circle circle1 pause-animation"/>
-                    <i className="material-icons server2 pause-animation">
-                      storage
-                    </i>
-                    <div className="circle circle2 pause-animation"/>
-                    <i className="material-icons server3 pause-animation">
-                      storage
-                    </i>
-                    <div className="circle circle3 pause-animation"/>
-                    <i className="material-icons server4 pause-animation">
-                      storage
-                    </i>
-                    <div className="circle circle4 pause-animation"/>
-                    <i className="material-icons server5 pause-animation">
-                      storage
-                    </i>
-                    <i className="material-icons server6 pause-animation">
-                      storage
-                    </i>
-                    <div className="circle circle6 pause-animation"/>
-                    <svg
-                      className="pause-animation"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink">
-                      <path d="M325,80 L70,173 M325,80 L413,173 M323,80 L323,303"/>
-                      <path d="M325,80 L163,303 M300,61 L190,61"/>
-                      <path d="M165,80 L165,305 M75,170 L165,80 M323,303 L165,80"/>
-                      <path d="M420,175 L165,83 M319,305 L415,209 M423,209 L165,300"/>
-                      <path d="M165,300 L70,205 M328,303 L55,205"/>
-                      <path d="M185,321 L300,321 M99,192.5 L390,192.5"/>
-                    </svg>
-                    <div className="moving shotting1 pause-animation"/>
-                    <div className="moving shotting2 pause-animation"/>
-                    <div className="moving shotting3 pause-animation"/>
-                    <div className="moving shotting4 pause-animation"/>
-                    <div className="moving shotting5 pause-animation"/>
-                    <i className="material-icons greenServer visable pause-animation">
-                      storage
-                    </i>
-                    <span className="majority pause-animation">Majority</span>
-                    <span className="score2 visable pause-animation">:4</span>
-                    <i className="material-icons redServer visable pause-animation">
-                      storage
-                    </i>
-                    <span className="score1 visable pause-animation">:2</span>
+      <section id="StepSix">
+        <div data-aos="fade-up" data-aos-delay="1000">
+          <div className="container" id="server-animation">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <div className="line"/>
+                <h1>SEND BLOCK</h1>
+                <div className="line"/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-8">
+                <div className="box stepSix">
+                  <div className="row">
+                    <div className="vert-center">
+                      <div className="col-md-8 text-center secondaryFont">
+                        <button
+                          href="#"
+                          className="play"
+                          onClick={this
+                          .playAnimation
+                          .bind(this)}>
+                          Play Animation
+                        </button>
+                        <button
+                          href="#"
+                          className="restart hidden"
+                          onClick={this
+                          .restartAnimation
+                          .bind(this)}>
+                          Restart Animation
+                        </button>
+                        <i className="material-icons server1 pause-animation">
+                          storage
+                        </i>
+                        <div className="circle circle1 pause-animation"/>
+                        <i className="material-icons server2 pause-animation">
+                          storage
+                        </i>
+                        <div className="circle circle2 pause-animation"/>
+                        <i className="material-icons server3 pause-animation">
+                          storage
+                        </i>
+                        <div className="circle circle3 pause-animation"/>
+                        <i className="material-icons server4 pause-animation">
+                          storage
+                        </i>
+                        <div className="circle circle4 pause-animation"/>
+                        <i className="material-icons server5 pause-animation">
+                          storage
+                        </i>
+                        <i className="material-icons server6 pause-animation">
+                          storage
+                        </i>
+                        <div className="circle circle6 pause-animation"/>
+                        <svg
+                          className="pause-animation"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlnsXlink="http://www.w3.org/1999/xlink">
+                          <path d="M325,80 L70,173 M325,80 L413,173 M323,80 L323,303"/>
+                          <path d="M325,80 L163,303 M300,61 L190,61"/>
+                          <path d="M165,80 L165,305 M75,170 L165,80 M323,303 L165,80"/>
+                          <path d="M420,175 L165,83 M319,305 L415,209 M423,209 L165,300"/>
+                          <path d="M165,300 L70,205 M328,303 L55,205"/>
+                          <path d="M185,321 L300,321 M99,192.5 L390,192.5"/>
+                        </svg>
+                        <div className="moving shotting1 pause-animation"/>
+                        <div className="moving shotting2 pause-animation"/>
+                        <div className="moving shotting3 pause-animation"/>
+                        <div className="moving shotting4 pause-animation"/>
+                        <div className="moving shotting5 pause-animation"/>
+                        <i className="material-icons greenServer visable pause-animation">
+                          storage
+                        </i>
+                        <span className="majority pause-animation">Majority</span>
+                        <span className="score2 visable pause-animation">:4</span>
+                        <i className="material-icons redServer visable pause-animation">
+                          storage
+                        </i>
+                        <span className="score1 visable pause-animation">:2</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className="col-md-4 vert-center">
+                <h1>
+                  The server which creates the block, then sends it out to all others on the
+                  network. So long as the majority recives and validate the block, it is then
+                  successfully added to the chain.
+                </h1>
+              </div>
             </div>
           </div>
-          <div className="col-md-4 vert-center">
-            <h1>
-              The server which creates the block, then sends it out to all others on the
-              network. So long as the majority recives and validate the block, it is then
-              successfully added to the chain.
-            </h1>
-          </div>
         </div>
-<<<<<<< HEAD
-        </div>
-      )
-    }
-=======
-      </div>
+      </section>
     );
->>>>>>> 791caee8fa2d8e382371de731ff0f13232306d76
   }
 }
 export default StepSix;
