f47697a35e9059314e215430881befcc0fc5abff
moogs <me@moogs.work>
2017-11-07 16:58:32 -0700

    Added production mode for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,46 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
+    'webpack-dev-server/client?http://localhost:5000', './src/index.jsx'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: __dirname,
     filename: 'bundle.js',
-    publicPath: '/build/'
+    publicPath: '/static/'
   },
+  resolve: {
+    extensions: ['.js']
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), new webpack.NoErrorsPlugin()
+  ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        loaders: ['babel-loader'],
         include: path.join(__dirname, 'src')
-      },
-      {
+      }, {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }, {
         test: /\.(png|jpeg|ttf)$/,
         use: [
-         { loader: 'url-loader', options: { limit: 8192 } }
-         // limit => file.size =< 8192 bytes ? DataURI : File
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+          // limit => file.size =< 8192 bytes ? DataURI : File
         ]
       }
     ]
   },
   node: {
-  fs: 'empty',
-  net: 'empty'
-}
+    fs: 'empty',
+    net: 'empty'
+  }
 };
