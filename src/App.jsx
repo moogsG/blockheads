5f3e8d9bba4c089a44ed47f8ca34204726aa9782
moogs <me@moogs.work>
2017-11-08 12:18:50 -0700

    Not picking up WS once hosted

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.recive = new WebSocket('ws://blockheadzchain.herokuapp.com');
+    this.recive = new WebSocket('wss://blockheadzchain.herokuapp.com');
 
     this.recive.onmessage = event => {
       console.log("got message");
