a4a3ae07cb91ea4e1926f589d6716c8a74f37153
moogs <me@moogs.work>
2017-11-29 22:00:11 -0700

    MK logo link added

--- src/presentational/navBar.jsx
+++ src/presentational/navBar.jsx
@@ -30,7 +30,7 @@ class navBar extends Component {
       <nav className="navbar fixed-top bg-faded">
         <div className="blocks">{block}</div>
         <div id="navLine"/>
-        <img className="img-fluid mk-logo" src="../../imgs/mk.png"/>
+        <a href="https://theminingking.com/"><img className="img-fluid mk-logo" src="../../imgs/mk.png"/></a>
       </nav>
     );
   }
